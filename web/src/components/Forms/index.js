8a9c1c391b87b5acce6b6be2a6b9dadf0a87dbeb
PedroBenedito1512 <pedro.00000844519@unicap.br>
2024-09-03 10:52:14 -0300

    forms login concluido

--- web/src/components/Forms/index.js
+++ web/src/components/Forms/index.js
@@ -3,22 +3,26 @@ import styles from "./Forms.module.css";
 function Forms(){
     return(
         <section className={styles.forms}>
-            <h1> Institulo Fenix </h1>
+            <h1> Instituto Fenix </h1>
             <p>Bem vindo de volta! Por favor insira as informações de login</p>
             <forms>
                 <div className = {styles.textinputs}>
-                    <label for = "email">Email</label>
+                    <label for = "email">E-mail</label>
                     <input type = "email" id = "email" name = "email" placeholder = "nome@gmail.com"></input>
                     <label for = "senha">Senha</label>
                     <input type = "password" id = "senha" name = "senha" placeholder = "********"></input>
                 </div> 
                 <div className={styles.links}>
-                    <input type="checkbox" id="lembrar" name="lembrar" value="true"></input>
-                    <label for="lembrar"> Lembrar-se da senha</label>
-                    <a href =" ">esqueceu senha?</a>
-                </div>   
-                <button type = "submit" name = "btlogin" id = "btlogin">Login</button>
-                <button type = "submit" name = "btregistro" id ="btregistro">Cadastre-se</button>
+                    <div className ={styles.conteinerlembrar}>
+                        <input type="checkbox" id="lembrar" name="lembrar" value="true"></input>
+                        <label for="lembrar">Lembrar-se da senha</label>
+                    </div>
+                    <a href =" "><strong>esqueceu senha?</strong></a>
+                </div>
+                <div className = {styles.buttonsLogin}>   
+                <button className = {styles.btlogin} type = "submit" name = "btlogin" id = "btlogin">Login</button>
+                <button className = {styles.btregistro} type = "submit" name = "btregistro" id ="btregistro">Cadastre-se</button>
+                </div>
 
             </forms>
             
