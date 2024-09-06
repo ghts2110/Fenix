0d33f326609d506a8fca920567ea38923fa57add
ghts2110 <gabrieltorres2110@gmail.com>
2024-09-06 13:27:09 -0300

    NavigatePanel

--- web/src/components/NavigatePanel/index.js
+++ web/src/components/NavigatePanel/index.js
@@ -1,19 +1,20 @@
 import styles from "./NavigatePanel.module.css";
 
-function NavigatePanel({imagem}){
-    return(
-        <section className ={styles.background}>
-             <div 
-                className={styles.banner}
-                style={{backgroundImage : `url('/imagems/${imagem}.png')`}}
-            >
-            </div >
-
-        </section>
-    );
-
+function NavigatePanel() {
+  return (
+    <section className={styles.NavigatePanel_bg}>
+      <div className={styles.NavigatePanelContainer}>
+        <h1>Fenix</h1>
+        <div className={styles.buttonsContainer}>
+          <button className={styles.button}>Painel de Controle</button>
+          <button className={styles.button}>Processos</button>
+          <button className={styles.button}>Cadastro do Cliente</button>
+          <button className={styles.button}>Solicitações</button>
+          <button className={styles.button}>Configurações</button>
+        </div>
+      </div>
+    </section>
+  );
 }
-
-
 
 export default NavigatePanel;
--- web/src/components/TelaDashboard/index.js
+++ web/src/components/TelaDashboard/index.js
@@ -1,10 +1,11 @@
 import styles from './TelaDashboard.module.css';
+
 import NavigatePanel from '../NavigatePanel';
 
 function TelaDashboard() {
     return (
         <main className={styles.TelaDashboard}>
-            <NavigatePanel></NavigatePanel>
+            <NavigatePanel className={styles.item1}/>
         </main>
     );
 }
