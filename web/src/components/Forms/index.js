3ddb7b2f324a577ddf4e733739428bbae58f1b63
ghts2110 <gabrieltorres2110@gmail.com>
2024-09-13 10:57:35 -0300

    test beackend

--- web/src/components/Forms/index.js
+++ web/src/components/Forms/index.js
@@ -1,32 +1,71 @@
 import styles from "./Forms.module.css";
+import React, { useState } from "react";
 
-function Forms(){
-    return(
-        <section className={styles.forms}>
-            <h1> Instituto Fenix </h1>
-            <p>Bem vindo de volta! Por favor insira as informações de login</p>
-            <forms>
-                <div className = {styles.textinputs}>
-                    <label for = "email">E-mail</label>
-                    <input type = "email" id = "email" name = "email" placeholder = "nome@gmail.com"></input>
-                    <label for = "senha">Senha</label>
-                    <input type = "password" id = "senha" name = "senha" placeholder = "********"></input>
-                </div> 
-                <div className={styles.links}>
-                    <div className ={styles.conteinerlembrar}>
-                        <input type="checkbox" id="lembrar" name="lembrar" value="true"></input>
-                        <label for="lembrar">Lembrar-se da senha</label>
-                    </div>
-                    <a href =" "><strong>esqueceu senha?</strong></a>
-                </div>
-                <div className = {styles.buttonsLogin}>   
-                    <button className = {styles.btlogin} type = "submit" name = "btlogin" id = "btlogin">Login</button>
-                </div>
 
-            </forms>
-            
-        </section>
-    );
+const Forms = () => {
+  const [login, setLogin] = useState("");
+  const [senha, setSenha] = useState("");
+  
+  const handleSubmit = (event) => {
+    
+    console.log("Envio");
+  };
+
+  return (
+    <section className={styles.forms}>
+      <h1> Instituto Fenix </h1>
+      <p>Bem vindo de volta! Por favor insira as informações de login</p>
+
+      <forms onSubmit={handleSubmit}>
+        <div className={styles.textinputs}>
+          <label for="email">E-mail</label>
+          <input
+            type="email"
+            name="email"
+            onChange={(e) => setLogin(e.target.value)}
+            id="email"
+            value={login}
+            placeholder="nome@gmail.com"
+          ></input>
+
+          <label for="senha">Senha</label>
+          <input
+            type="password"
+            name="password"
+            onChange={(e) => setSenha(e.target.value)}
+            id="senha"
+            value={senha}
+            placeholder="********"
+          ></input>
+        </div>
+
+        <div className={styles.links}>
+          <div className={styles.conteinerlembrar}>
+            <input
+              type="checkbox"
+              id="lembrar"
+              name="lembrar"
+              value="true"
+            ></input>
+            <label for="lembrar">Lembrar-se da senha</label>
+          </div>
+          <a href=" ">
+            <strong>esqueceu senha?</strong>
+          </a>
+        </div>
+        <div className={styles.buttonsLogin}>
+          <button
+            className={styles.btlogin}
+            type="submit"
+            name="btlogin"
+            id="btlogin"
+          >
+            Login
+          </button>
+        </div>
+      </forms>
+    </section>
+  );
 }
 
 export default Forms;
