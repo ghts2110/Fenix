5eade036f9a640ddb45ea649508a4c7b6a403601
ghts2110 <gabrieltorres2110@gmail.com>
2024-09-13 15:53:37 -0300

    Lembrar-se da senha feature/login

--- web/src/App.js
+++ web/src/App.js
@@ -1,16 +1,32 @@
 import TelaLogin from "./components/TelaLogin";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TelaDashboard from "./components/TelaDashboard";
+import { CookiesProvider, useCookies } from "react-cookie";
 
 function App() {
+  const [cookies, setCookie] = useCookies(["user"]);
+
+  function handleLogin(user) {
+    setCookie("user", user, { path: "/" });
+  }
+
   return (
     <Router>
+      <CookiesProvider>
+          <div>
+            {cookies.user ? (
+              <TelaLogin user={cookies.user} />
+            ) : (
+              <TelaDashboard onLogin={handleLogin} />
+            )}
+          </div>
+        </CookiesProvider>
+
       <Routes>
         <Route path="/" element={<TelaLogin />} />
         <Route path="/dashboard" element={<TelaDashboard />} />
       </Routes>
     </Router>
-
   );
 }
 
