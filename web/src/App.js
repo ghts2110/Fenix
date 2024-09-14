e6439efe92225c17dd9e320ddf2fb00a298e5971
princevegeta <princevegeta@kingkong.com>
2024-09-14 15:30:59 -0300

    feat (tela de client) criação da tela de cadastro do client

--- web/src/App.js
+++ web/src/App.js
@@ -1,16 +1,37 @@
 import TelaLogin from "./components/TelaLogin";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import TelaDashboard from "./components/TelaDashboard";
+import { CookiesProvider, useCookies } from "react-cookie";
+import Register from "./components/TelaRegister";
+import Client from "./components/TelaClient";
 
 function App() {
+  const [cookies, setCookie] = useCookies(["user"]);
+
+  function handleLogin(user) {
+    setCookie("user", user, { path: "/" });
+  }
+
   return (
     <Router>
+      {/* <CookiesProvider>
+          <div>
+            {cookies.user ? (
+              <TelaLogin user={cookies.user} />
+            ) : (
+              <TelaDashboard onLogin={handleLogin} />
+            )}
+          </div>
+        </CookiesProvider> */}
+
       <Routes>
         <Route path="/" element={<TelaLogin />} />
         <Route path="/dashboard" element={<TelaDashboard />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/client" element={<Client />} />
+
       </Routes>
     </Router>
-
   );
 }
 
---
+++ web/src/components/TelaClient/BoxClient/index.js
@@ -0,0 +1,11 @@
+import styles from "./BoxClient.module.css";
+
+const Client = () => {
+  return (
+    <section className={styles.Client}>
+ 
+    </section>
+  );
+};
+
+export default Client;
