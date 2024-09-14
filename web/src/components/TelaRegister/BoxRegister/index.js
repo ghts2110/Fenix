43136e515e2f573cc950d9f7379b692cf732f519
ghts2110 <gabrieltorres2110@gmail.com>
2024-09-14 19:19:08 -0300

    tela de cadastros vinculado ao banco feature/register

--- web/src/components/TelaRegister/BoxRegister/index.js
+++ web/src/components/TelaRegister/BoxRegister/index.js
@@ -1,30 +1,36 @@
 import styles from "./BoxRegister.module.css";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Register = () => {
-
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    position: '',
-    cpf: '',
-    password: '',
-    birthDate: '',
-  });
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+  // criar resistro
+  const urlBase = "https://parseapi.back4app.com/classes/assistido";
+  const headersJson = {
+    "X-Parse-Application-Id": "9oVDtFSi4LvkNyv1ORv3Yy3Xb59v4GpMQLMwpKzt",
+    "X-Parse-REST-API-Key": "ewQW6PmSaxcJaSTOC5z1iKKBv1P3YzdYU8D72Ump",
   };
+  const addbanck = async (event) => {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const position = document.getElementById("position").value;
+    const cpf = document.getElementById("cpf").value;
+    const password = document.getElementById("password").value;
+    const birthDay = document.getElementById("birthDay").value;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData);
-    // Aqui você pode implementar a lógica de envio, como uma requisição HTTP
+    const response = await fetch(urlBase, {
+      method: "POST",
+      
+      headers: headersJson,
+      body: JSON.stringify({name:name, email: email, password: password, position:position, cpf:cpf, birthDay:birthDay}),
+    });
+    const data = await response.json();
+    console.log(response);
+    console.log(data);
   };
 
   return (
     <section className={styles.Register}>
-      <form onSubmit={handleSubmit}>
+      <form>
         <div className={styles.formGroup1}>
           <div className={styles.boxName}>
             <label htmlFor="name">Nome Completo</label>
@@ -33,12 +39,10 @@ const Register = () => {
               name="name"
               id="name"
               placeholder="Coloque seu nome"
-              value={formData.name}
-              onChange={handleChange}
               required
             />
           </div>
-          
+
           <div className={styles.boxEmail}>
             <label htmlFor="email">E-mail</label>
             <input
@@ -46,11 +50,9 @@ const Register = () => {
               name="email"
               id="email"
               placeholder="Coloque seu e-mail"
-              value={formData.email}
-              onChange={handleChange}
               required
             />
-          </div>          
+          </div>
         </div>
 
         <div className={styles.formGroup1}>
@@ -61,8 +63,6 @@ const Register = () => {
               name="position"
               id="position"
               placeholder="Coloque o cargo que você deseja"
-              value={formData.position}
-              onChange={handleChange}
               required
             />
           </div>
@@ -74,13 +74,11 @@ const Register = () => {
               name="cpf"
               id="cpf"
               placeholder="Coloque seu CPF"
-              value={formData.cpf}
-              onChange={handleChange}
               required
             />
           </div>
         </div>
-          
+
         <div className={styles.formGroup1}>
           <div className={styles.boxPassword}>
             <label htmlFor="password">Senha</label>
@@ -89,26 +87,24 @@ const Register = () => {
               name="password"
               id="password"
               placeholder="Coloque sua senha"
-              value={formData.password}
-              onChange={handleChange}
               required
             />
           </div>
 
-          <div className={styles.boxPassword}>
-            <label htmlFor="birthDate">Data de Nascimento</label>
+          <div className={styles.boxBirthDay}>
+            <label htmlFor="birthDay">Data de Nascimento</label>
             <input
               type="date"
-              name="birthDate"
-              id="birthDate"
-              value={formData.birthDate}
-              onChange={handleChange}
+              name="birthDay"
+              id="birthDay"
               required
             />
           </div>
         </div>
-          
-        <button className={styles.btResister} type="submit">Cadastrar</button>
+
+        <button className={styles.btResister} type="submit" onClick={addbanck}>
+          Cadastrar
+        </button>
       </form>
     </section>
   );
