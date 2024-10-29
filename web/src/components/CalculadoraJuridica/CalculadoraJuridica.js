0a153fc705615aadf8982de4caecd9c6129dda3b
PedroBenedito1512 <pedro.00000844519@unicap.br>
2024-10-29 16:20:32 -0300

    feat: calculadora juridica

---
+++ web/src/components/CalculadoraJuridica/CalculadoraJuridica.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import styles from "./CalculadoraJuridica.module.css";
+import FGTSCalculator from "./FGTSCalculator";
+import RescisaoCalculator from "./RescisaoCalculator";
+
+function CalculadoraJuridica() {
+  const [selectedCalc, setSelectedCalc] = useState("fgts");
+
+  const renderCalculator = () => {
+    switch (selectedCalc) {
+      case "fgts":
+        return <FGTSCalculator />;
+      case "rescisao":
+        return <RescisaoCalculator />;
+      default:
+        return <FGTSCalculator />;
+    }
+  };
+
+  return (
+    <div className={styles.calculadoraContainer}>
+      <h2>Calculadora Jurídica</h2>
+      <div className={styles.selectorContainer}>
+        <button onClick={() => setSelectedCalc("fgts")}>Cálculo FGTS</button>
+        <button onClick={() => setSelectedCalc("rescisao")}>Cálculo de Rescisão</button>
+      </div>
+      <div className={styles.calculatorContent}>
+        {renderCalculator()}
+      </div>
+    </div>
+  );
+}
+
+export default CalculadoraJuridica;
